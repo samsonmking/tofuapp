5c2d47350eb2d7881e73174cc9b3c0bffe132cf3
samsonmking <samson@samsonmking.com>
2019-05-04 15:22:04 -0400

    Enable CORS in dev mode

--- app/app.ts
+++ app/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import { Route } from './route';
 import { StaticResource } from './static-resource';
 
@@ -8,9 +9,11 @@ export const buildApp = (routes: Route[], staticResources: StaticResource[]): ex
     app.set('port', process.env.PORT || 3000);
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
-
+    if(process.env.NODE_ENV === 'dev') {
+        console.log('[WARNING] Cross origin requests enabled in dev mode');
+        app.use(cors());
+    }
     routes.forEach(r => r.contributeRoutes(app));
     staticResources.forEach(r => r.contributeStatic(app));
-
     return app;
 }
