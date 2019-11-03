0f7d3c6cb056913d7a2dc791d7544473481f992c
samsonmking <samson@samsonmking.com>
2019-11-03 08:26:57 -0500

    Add new list button in list-selection-component

--- src/app/lists/list-selection/list-selection.component.ts
+++ src/app/lists/list-selection/list-selection.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ShoppingListFacade } from 'src/app/core-data/state/shopping-list/shopping-list.facade';
 import { Observable } from 'rxjs';
 import { ShoppingList } from 'src/app/core-data/models/shopping-list/shopping-list';
+import { MatDialog } from '@angular/material';
+import { AddListComponent } from 'src/app/shared/add-list/add-list.component';
 
 @Component({
   selector: 'app-list-selection',
@@ -12,7 +14,9 @@ export class ListSelectionComponent implements OnInit {
   lists$: Observable<ShoppingList[]>;
   defaultListId$: Observable<number>;
 
-  constructor(private readonly listFacade: ShoppingListFacade) { }
+  constructor(
+    private readonly listFacade: ShoppingListFacade,
+    private readonly matDialog: MatDialog) { }
 
   ngOnInit() {
     this.lists$ = this.listFacade.lists$;
@@ -22,6 +26,12 @@ export class ListSelectionComponent implements OnInit {
 
   deleteList(id: number) {
     this.listFacade.deleteShoppingList(id);
+  }
+
+  createNewList(){
+    this.matDialog.open(AddListComponent, {
+      width: "400px",
+    });
   }
 
 }
