8e7881f99bf8b4cb9b1e76797c5d7d7305aeb4b9
samsonmking <samson@samsonmking.com>
2019-12-08 12:13:01 -0500

    Migrate from config file enviroment variables

--- backend-api/app/recipe-image/fs-recipe-image-store.ts
+++ backend-api/app/recipe-image/fs-recipe-image-store.ts
@@ -2,12 +2,12 @@ import { RecipeImageStore } from "./recipe-image-store";
 import { RecipeImageConverter, ImageConversionResult } from "./recipe-image-converter";
 import request from 'request';
 import fs from 'fs';
-import { imagesRoot } from "./config";
 import path from 'path';
 import sharp = require("sharp");
+const { imagesRoot } = process.env;
 
 export class FileRecipeImageStore implements RecipeImageStore, RecipeImageConverter {
-    constructor(private outDir: string = imagesRoot) {
+    constructor(private outDir = imagesRoot) {
 
     }
 
