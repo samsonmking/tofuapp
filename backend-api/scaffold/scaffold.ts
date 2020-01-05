d1baaf8d6f5d776fb56e48b7c8d15bf8fd0b1467
samsonmking <samson@samsonmking.com>
2020-01-05 14:13:38 -0500

    Increase image thumbnail size
    + Save images as 800 x 600
    + Fix bug where thumbnails would push off the screen with iPad -
    portrait

--- backend-api/scaffold/scaffold.ts
+++ backend-api/scaffold/scaffold.ts
@@ -1,36 +1,8 @@
 import { createDatabase, dropDatabase } from "../app/db/create-schema";
 import { dispose } from "../app/db/index";
-import fs from 'fs';
-import { Recipe } from "../app/recipe";
-import { RecipePSRepo } from "../app/recipe/recipe-ps-repo";
-import { IngredientPSRepo } from "../app/ingredient/persistance/ingredient-ps-repo";
 import { userScaffold } from "../app/user";
 import { createUser } from "../app/user/scaffold";
 const { PGDATABASE } = process.env;
-
-async function importDataFromFile(userId: string, path: string) {
-    const payload: string = fs.readFileSync(path).toString();
-    await importData(userId, payload);
-}
-
-async function importData(userId: string, payload: string) {
-    const recipeRepo = new RecipePSRepo();
-    const ingredientRepo = new IngredientPSRepo();
-
-    const recipes: Recipe[] = JSON.parse(payload);
-    for (const recipe of recipes) {
-        const dbRecipe = await recipeRepo.addRecipe({
-            name: recipe.name,
-            url: recipe.url,
-            user_id: userId
-        });
-        const recipeId = dbRecipe.id;
-        const ingredients = recipe.ingredients.map(i => ({ ...i, recipe_id: recipeId }));
-        await ingredientRepo.addIngredients(ingredients);
-    }   
-}
-
-const seed = './scaffold/recipe-store.json';
 
 console.log('[INFO] scaffolding database');
 dropDatabase()
@@ -42,8 +14,6 @@ dropDatabase()
         console.log(`[INFO] Default user '${user}' created`); 
         return user; 
     })
-    .then(user => importDataFromFile(user, seed))    
-    .then(() => console.log(`[INFO] Database ${PGDATABASE} seeded with data from ${seed}`))
     .then(() => createUser('user@gmail.com', 'password'))
     .then(() => dispose())
     .catch(e => console.log(`[ERROR] ${e}`));
