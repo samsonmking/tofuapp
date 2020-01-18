e3cf8df67dc98135b7b407a4df59120d539c7f9b
samsonmking <samson@samsonmking.com>
2020-01-18 18:44:21 -0500

    Recipe + Shopping List Reactive Layout
    + Reactive font sizing
    + Make toolbar reactive
    + Recipe -> Default shopping list off screen on mobile / sm devices
    + List -> List selection off screen on mobile / sm devices

--- frontend/src/app/lists/lists.component.ts
+++ frontend/src/app/lists/lists.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { MediaObserver } from '@angular/flex-layout';
+import { mergeAll, map, filter, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-lists',
@@ -6,10 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lists.component.css']
 })
 export class ListsComponent implements OnInit {
+  sideNavConfig$: Observable<SideNavConfig>;
 
-  constructor() { }
+  constructor(private readonly mediaObserver: MediaObserver) {
+
+    const mqAlias$ = this.mediaObserver.asObservable().pipe(
+      mergeAll(),
+      map(mq => mq.mqAlias)
+    );
+
+    this.sideNavConfig$ = mqAlias$.pipe(
+      filter(alias => alias.length === 2),
+      map(alias => {
+        switch(alias) {
+          case 'xs':
+            return { open: false, mode: 'over' }
+          case 'sm':
+            return { open: false, mode: 'over' }
+          default:
+            return { open: true, mode: 'side' }
+        }
+      }),
+      distinctUntilChanged((curr, prev) => 
+        curr.mode === prev.mode && curr.open === prev.open)
+    );
+
+   }
 
   ngOnInit() {
   }
 
+}
+
+export interface SideNavConfig {
+  open: boolean;
+  mode: string;
 }
