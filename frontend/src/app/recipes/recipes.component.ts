d1c0e86b8506a26bcda42a801af85d00c4cf04f8
samsonmking <samson@samsonmking.com>
2020-01-18 18:44:21 -0500

    Recipe + Shopping List Reactive Layout
    + Reactive font sizing
    + Make toolbar reactive
    + Recipe -> Default shopping list off screen on mobile / sm devices
    + List -> List selection off screen on mobile / sm devices

--- frontend/src/app/recipes/recipes.component.ts
+++ frontend/src/app/recipes/recipes.component.ts
@@ -4,8 +4,9 @@ import { Observable, combineLatest } from 'rxjs';
 import { DisplayRecipe } from '../core-data/models/recipe/display-recipe';
 import { MatDialog } from '@angular/material';
 import { ManualEntryComponent } from './add-recipe/manual-entry/manual-entry.component';
-import { map, filter, mergeAll, distinctUntilChanged } from 'rxjs/operators';
+import { map, filter, mergeAll, distinctUntilChanged, startWith } from 'rxjs/operators';
 import { MediaObserver } from '@angular/flex-layout';
+import { ShoppingListFacade } from '../core-data/state/shopping-list/shopping-list.facade';
 
 
 @Component({
@@ -16,23 +17,34 @@ import { MediaObserver } from '@angular/flex-layout';
 })
 export class RecipesComponent implements OnInit {
   recipes$: Observable<DisplayRecipe[][]>;
-  viewChange$:Observable<any>;
+  sideNavConfig$: Observable<SideNavConfig>;
+  currentListName$: Observable<string>
+
 
   constructor(private fascade: RecipeFacade,
+    private listFacade: ShoppingListFacade,
     private dialog: MatDialog,
     private readonly mediaObserver: MediaObserver) {
 
-      const chunkSize$ = this.mediaObserver.asObservable().pipe(
+      this.currentListName$ = this.listFacade.currentList$.pipe(
+        map(list => list.name)
+      )
+
+      const mqAlias$ = this.mediaObserver.asObservable().pipe(
         mergeAll(),
-        map(mq => mq.mqAlias),
+        map(mq => mq.mqAlias)
+      );
+
+      const chunkSize$ = mqAlias$.pipe(
         filter(alias => alias.length === 2),
         map(alias => {
             switch (alias) {
                 case 'xs':
-                case 'sm':
                     return 1;
-                case 'md':
+                case 'sm':
                     return 2;
+                case 'md':
+                    return 3;
                 default:
                     return 4;
             }
@@ -40,10 +52,25 @@ export class RecipesComponent implements OnInit {
         distinctUntilChanged()
     );
 
+    this.sideNavConfig$ = mqAlias$.pipe(
+      filter(alias => alias.length === 2),
+      map(alias => {
+        switch(alias) {
+          case 'xs':
+            return { open: false, mode: 'over' }
+          case 'sm':
+            return { open: false, mode: 'over' }
+          default:
+            return { open: true, mode: 'side' }
+        }
+      }),
+      distinctUntilChanged((curr, prev) => 
+        curr.mode === prev.mode && curr.open === prev.open)
+    );
+
     this.recipes$ = combineLatest(this.fascade.recipes$, chunkSize$).pipe(
         map(([recipes, size]) => this.chunk(recipes, size))
     );
-   
   }
 
   chunk<T>(arr: Array<T>, chunkSize: number) {
@@ -63,3 +90,7 @@ export class RecipesComponent implements OnInit {
   }
 }
 
+export interface SideNavConfig {
+  open: boolean;
+  mode: string;
+}
