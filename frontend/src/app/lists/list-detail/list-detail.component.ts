dd098a2ea9256be2d8270218e6095fa8a1149144
samsonmking <samson@samsonmking.com>
2020-01-26 20:37:37 -0500

    Refactor list reactive layout
    + switch from table to flexbox divs

--- frontend/src/app/lists/list-detail/list-detail.component.ts
+++ frontend/src/app/lists/list-detail/list-detail.component.ts
@@ -1,15 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ShoppingListItemFacade } from 'src/app/core-data/state/shopping-list-item/shopping-list-items.facade';
-import { map, filter, switchMap, first, takeUntil, publishReplay, share, refCount, shareReplay } from 'rxjs/operators';
-import { Observable, combineLatest, BehaviorSubject, Subscription } from 'rxjs';
-import { IngredientFacade } from 'src/app/core-data/state/ingredient/ingredient.facade';
-import { RecipeFacade } from 'src/app/core-data/state/recipe/recipes.facade';
+import { Observable } from 'rxjs';
 import { Sort } from '@angular/material/sort';
-import { Actions, ofType } from '@ngrx/effects';
-import { GetIngredientsForCurrentListComplete, IngredientActionsType, GetIngredientsForCurrentListRequest } from 'src/app/core-data/state/ingredient/ingredient.actions';
-import { SetDefaultList, ShoppingListActionTypes } from 'src/app/core-data/state/shopping-list/shopping-list.actions';
 import { DisplayListItem } from 'src/app/core-data/models/shopping-list-item/display-list-item';
 import { ListSortService } from './list-sort-service';
+import { MatSelectionListChange } from '@angular/material';
 
 @Component({
   selector: 'app-list-detail',
@@ -34,6 +29,10 @@ export class ListDetailComponent implements OnInit {
 
   itemChecked(id: number, isChecked: boolean) {
     this.listItemsFacade.checkListItem(id, isChecked);
+  }
+
+  selectionChanged(event: MatSelectionListChange) {
+    this.listItemsFacade.checkListItem(event.option.value.itemId, event.option.selected);
   }
 
 }
